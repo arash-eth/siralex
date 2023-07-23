92cb3f129fddeb2d1a3592f3fdeab1d619dbc6d8
arash-eth <96187004+arash-eth@users.noreply.github.com>
2023-07-23 12:51:24 +0330

    Update index.js
    Update Contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x82db0485699aaa15cbf7bae6241a35cc28a99120";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
